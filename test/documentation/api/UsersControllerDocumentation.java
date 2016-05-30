44f69b9878d33614de20c30184fb53bd4daa962a
Miroslav Matejovsky <miroslav.matejovsky@wirecard.com>
2016-05-30 15:51:06 +0200

    added simple post with rest-assured

--- test/documentation/api/UsersControllerDocumentation.java
+++ test/documentation/api/UsersControllerDocumentation.java
@@ -1,6 +1,9 @@
 package documentation.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import play.test.WithServer;
@@ -16,6 +19,27 @@ public class UsersControllerDocumentation extends WithServer {
       .get("/")
     .then()
       .body(Matchers.equalTo("Play API documentation with Slate"));
+  }
+
+  @Test
+  public void customerCreationDocumentation() {
+
+    final ObjectMapper mapper = new ObjectMapper();
+    final ObjectNode request = mapper.createObjectNode()
+        .put("nickname", "some-nickname")
+        .put("email", "some@email.com")
+        .put("phone-number", "1234");
+
+
+    RestAssured.given()
+        .port(port)
+        .log().all()
+        .contentType(ContentType.JSON)
+        .body(request)
+    .when()
+        .post("/api/users")
+    .then()
+        .statusCode(200);
   }
 
 }
